029996a440fcda34fe99cc17720cf5bedd4f0c00
Elbek Khakimbekov <hakimbekovelbek@gmail.com>
2022-08-29 16:20:53 +0500

    Code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { Banner, NavigationBar } from 'components'
+import { Banner, NavigationBar, Container } from 'components'
 import React from 'react'
 
 const App: React.FC = () => {
 	return (
-		<>
+		<Container>
 			<Banner />
 			<NavigationBar />
-		</>
+		</Container>
 	)
 }
 
--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,28 +1,25 @@
 import React from 'react'
-import { Container } from 'components'
 import { NAVBAR_CONFIG } from '../../constants'
 // @ts-ignore
 import styles from './NavigationBar.module.css'
 
 export const NavigationBar: React.FC = () => (
-	<Container>
-		<div className={styles.header_nav_menu}>
-			<ul className={styles.nav_menu_list}>
-				{NAVBAR_CONFIG.map(item => (
-					<li className={styles.menu_list_item} key={item.label}>
-						<a href={`/${item.link}`} className={styles.menu_list_item_link}>
-							{item.label}
-						</a>
-					</li>
-				))}
-			</ul>
-			<div className='menu_right'>
-				<div className='navBtn'>
-					<button className='menuBtn' type='button'>
-						Корзина
-					</button>
-				</div>
+	<div className={styles.header_nav_menu}>
+		<ul className={styles.nav_menu_list}>
+			{NAVBAR_CONFIG.map(item => (
+				<li className={styles.menu_list_item} key={item.label}>
+					<a href={`/${item.link}`} className={styles.menu_list_item_link}>
+						{item.label}
+					</a>
+				</li>
+			))}
+		</ul>
+		<div className='menu_right'>
+			<div className='navBtn'>
+				<button className='menuBtn' type='button'>
+					Корзина
+				</button>
 			</div>
 		</div>
-	</Container>
+	</div>
 )
